b8de4766f603b186ac3213ebea6fe112dc23a3e8
Andreas Isidorsson <isidorssona@gmail.com>
2024-04-12 10:55:31 +0200

    Refactor CartContext component and add addToCart functionality

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
+
+import { getAdditionalUserInfo } from "firebase/auth";
 
 interface Product {
   id: number;
@@ -11,11 +13,13 @@ interface Product {
 interface CartContextType {
   cartItems: Product[];
   setCartItems: React.Dispatch<React.SetStateAction<Product[]>>;
+  addToCart: (product: Product) => void;
 }
 
 const defaultCartContext: CartContextType = {
   cartItems: [],
-  setCartItems: () => {}, // provide a default function
+  setCartItems: () => {},
+  addToCart: () => {},
 };
 
 export const CartContext = createContext<CartContextType>(defaultCartContext);
@@ -27,11 +31,28 @@ interface CartProviderProps {
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [cartItems, setCartItems] = useState<Product[]>([]);
 
-  const value = { cartItems, setCartItems };
+  const value = {
+    cartItems,
+    setCartItems,
+    addToCart: (product: Product) => {
+      const existingProduct = cartItems.find((item) => item.id === product.id);
 
-  return (
-    <CartContext.Provider value={value}>
-      {children}
-    </CartContext.Provider>
-  );
+      if (existingProduct) {
+        setCartItems((prevItems) =>
+          prevItems.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        );
+      } else {
+        setCartItems((prevItems) => [
+          ...prevItems,
+          { ...product, quantity: 1 },
+        ]);
+      }
+    },
+  };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
