c532227830dc9f12caaae7f8c1a38e983a5959ca
Andreas Isidorsson <isidorssona@gmail.com>
2024-04-15 10:39:53 +0200

    test rolehandling

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,15 +1,16 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
-import { User } from 'firebase/auth';
-import { auth } from '../firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { sendPasswordResetEmail } from 'firebase/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { User } from "firebase/auth";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 interface AuthContextValue {
   isAuthenticated: boolean;
   currentUser: User | null;
+  role: string;
   signup: (email: string, password: string) => Promise<void>;
   login: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
@@ -22,59 +23,69 @@ interface AuthProviderProps {
   children: React.ReactNode;
 }
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState<string>("");
   const navigate = useNavigate();
 
-  
   function signup(email: string, password: string) {
     if (!email || !password) {
-      throw new Error('Email and password are required');
+      throw new Error("Email and password are required");
     }
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
-  function login(email: string, password: string) {
+  async function login(email: string, password: string) {
     if (!email || !password) {
-      throw new Error('Email and password are required');
+      throw new Error("Email and password are required");
     }
-    return signInWithEmailAndPassword(auth, email, password);
-  }
 
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    //set role to admin if email is the same as the admin email testing purposes
+    const role = email === "isidorssona@gmail.com" ? "admin" : "user";
+
+    setCurrentUser(user);
+    setRole(role);
+    setIsAuthenticated(true);
+  }
   function logout() {
-    navigate('/login');
+    navigate("/login");
     return auth.signOut();
   }
 
   function resetPassword(email: string) {
     if (!email) {
-      throw new Error('Email is required');
+      throw new Error("Email is required");
     }
     return sendPasswordResetEmail(auth, email);
   }
-
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
       setIsAuthenticated(!!user);
+
+      // Check if user is admin
+      const role = user?.email === "isidorssona@gmail.com" ? "admin" : "user";
+      setRole(role);
+
       setLoading(false);
     });
 
     return unsubscribe;
   }, []);
-
-
-
-// useEffect(() => {
-//   const token = localStorage.getItem('authToken');
-//   if (token) {
-//     setIsAuthenticated(true);
-//   }
-// }, []);
-
+  // useEffect(() => {
+  //   const token = localStorage.getItem('authToken');
+  //   if (token) {
+  //     setIsAuthenticated(true);
+  //   }
+  // }, []);
 
   const value = {
     isAuthenticated,
@@ -82,7 +93,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     signup,
     login,
     logout,
-    resetPassword
+    resetPassword,
+    role,
   };
 
   return (
